6b8c6aa83c741c3554ff6ba0041e2610edb166ba
Hayden Lowry <haylow77@gmail.com>
2023-03-23 11:44:58 -0600

    Push Progress to Github

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import teamsJson from './teams.json';
+
+const teamsList = teamsJson["teams"]
+
+class Team extends React.Component {
+  render() {
+    const oneTeam = this.props;
+    return(
+      <div>
+        <h2>{oneTeam}</h2>
+        <p>Mascot: {oneTeam.name}</p>
+        <p>Location: {oneTeam.city}, {oneTeam.state}</p>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      <Team {...teamsList} />
+    </div>
+  )
+}
+
+function Header() {
+  return (<h1>NCAA Baskeball Teams</h1>);
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header/>
       </header>
+      <body>
+        <TeamList/>
+      </body>
     </div>
   );
 }
